7cd8190721858274ebbaa04580059bd3244c2202
8Cookie9 <jaakko.koli@gmail.com>
2017-03-09 02:39:38 +0200

    web

---
+++ TeaPotFilling/src/main/java/com/database/UserDao.java
@@ -0,0 +1,24 @@
+package com.database;
+
+import com.poro.User;
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserDao implements Dao<User, Integer>{
+
+    @Override
+    public User findOne(Integer key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<User> findAll() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Integer key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- TeaPotFilling/src/main/java/com/teapotfilling/App.java
+++ TeaPotFilling/src/main/java/com/teapotfilling/App.java
@@ -1,5 +1,13 @@
 package com.teapotfilling;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import spark.ModelAndView;
+import spark.Spark;
+import static spark.Spark.*;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+import com.database.Database;
 import com.datahandling.Command;
 import com.datahandling.Data;
 import com.datahandling.GetData;
@@ -10,13 +18,26 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.SwingUtilities;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Data data=new Data("C:/Users/Jaakko/AppData/Roaming/AnkhHeart/AnkhBotR2/Twitch/Files");
-        Command command=new Command(data);
-        GUI gui=new GUI(command);
-        SwingUtilities.invokeLater(gui);
+public class App {
+    public static void main( String[] args ) throws Exception {
+//        Data data=new Data("C:/Users/Jaakko/AppData/Roaming/AnkhHeart/AnkhBotR2/Twitch/Files");
+//        Command command=new Command(data);
+//        GUI gui=new GUI(command);
+//        SwingUtilities.invokeLater(gui);
+        
+        if (System.getenv("PORT") != null) {
+            port(Integer.valueOf(System.getenv("PORT")));
+        }
+
+        String jdbcOsoite = "jdbc:sqlite:kanta1.db";
+        if (System.getenv("DATABASE_URL") != null) {
+            jdbcOsoite = System.getenv("DATABASE_URL");
+        }
+        Database database = new Database(jdbcOsoite);
+        
+        Spark.get("/", (req, res) -> {
+            res.redirect("/");
+            return "ok";
+        });
     }
 }
